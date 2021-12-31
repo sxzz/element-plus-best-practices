6618dd3295c9f97ede152ba671ce0982afb4d242
Kevin <sxzz@sxzz.moe>
2021-12-31 13:23:31 +0800

    chore: upgrade dependencies

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,33 @@
 // 方式 1:手动导入并注册图标组件
 import IconEpApple from '~icons/ep/apple'
 
+const msg = ref('')
+
 const handleClick = () => {
   ElMessage.success('Hello world')
 }
 </script>
 
 <template>
-  <IconEpApple />
+  <el-space direction="vertical">
+    <IconEpApple />
 
-  <!-- Method 2: or we can use `unplugin-vue-components` to auto register icon component -->
-  <!-- 或者我们可以使用 `unplugin-vue-components` 插件来自动注册图标 -->
-  <!-- Please refer to vite.config.ts -->
-  <!-- 请参阅 vite.config.ts -->
-  <i-ep-add-location />
-  <i-ep-aim />
+    <!-- Method 2: or we can use `unplugin-vue-components` to auto register icon component -->
+    <!-- 或者我们可以使用 `unplugin-vue-components` 插件来自动注册图标 -->
+    <!-- Please refer to vite.config.ts -->
+    <!-- 请参阅 vite.config.ts -->
+    <i-ep-add-location />
+    <i-ep-aim />
 
-  <!-- For more icons please refer to https://icones.js.org/ -->
-  <!-- 更多图标请参阅 https://icones.js.org/ -->
+    <!-- For more icons please refer to https://icones.js.org/ -->
+    <!-- 更多图标请参阅 https://icones.js.org/ -->
 
-  <div>
     <el-button @click="handleClick">
-      <el-icon><i-ep-circle-check-filled /></el-icon>
-      Hello world
+      <el-icon><i-ep-circle-check-filled /></el-icon> Hello world
     </el-button>
-  </div>
+
+    <el-input v-model="msg" />
+  </el-space>
 
   <div v-loading="true" style="height: 500px">Loading Area</div>
 </template>
--- src/components.d.ts
+++ src/components.d.ts
@@ -6,6 +6,8 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElSpace: typeof import('element-plus/es')['ElSpace']
     IEpAddLocation: typeof import('~icons/ep/add-location')['default']
     IEpAim: typeof import('~icons/ep/aim')['default']
     IEpCircleCheckFilled: typeof import('~icons/ep/circle-check-filled')['default']
