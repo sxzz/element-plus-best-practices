c5962d5f9a4a34a9b4fc8159fcaf81d227f492bc
三咲智子 <sxzz@sxzz.moe>
2022-06-13 14:23:37 +0800

    refactor: improve auto import

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 // Method 1: Import and register icon component manually.
-// 方式 1:手动导入并注册图标组件
+// 方式 1：手动导入并注册图标组件
 import IconEpApple from '~icons/ep/apple'
 
 let msg = $ref('')
@@ -14,8 +14,8 @@ const handleClick = () => {
 <script lang="ts">
 export default defineComponent({
   components: {
-    // Method 3: or we can use `unplugin-auto-import` to auto import icon component and register manally. not recommended)
-    // 方法 3：或者我们还可以使用 `unplugin-auto-import` 来自动导入组件，再手动注册组件。（不推荐）
+    // Method 2: or we can use `unplugin-auto-import` to auto import icon component and register manally. not recommended)
+    // 方法 2：或者我们还可以使用 `unplugin-auto-import` 来自动导入组件，再手动注册组件。（不推荐）
     IconEpRefresh,
   },
 })
@@ -25,16 +25,16 @@ export default defineComponent({
   <el-space direction="vertical">
     <IconEpApple />
 
-    <!-- Method 2: or we can use `unplugin-vue-components` to auto register icon component -->
-    <!-- 或者我们可以使用 `unplugin-vue-components` 插件来自动注册图标 -->
+    <!-- Method 2 -->
+    <!-- 方法 2 -->
+    <IconEpRefresh />
+
+    <!-- (recommended) Method 3: or we can use `unplugin-vue-components` to auto register icon component -->
+    <!-- （推荐）方法 3：我们可以使用 `unplugin-vue-components` 插件来自动注册图标 -->
     <!-- Please refer to vite.config.ts -->
     <!-- 请参阅 vite.config.ts -->
     <i-ep-add-location />
     <i-ep-aim />
-
-    <!-- Method 3 -->
-    <!-- 方法 3 -->
-    <IconEpRefresh />
 
     <!-- For more icons please refer to https://icones.js.org/ -->
     <!-- 更多图标请参阅 https://icones.js.org/ -->
