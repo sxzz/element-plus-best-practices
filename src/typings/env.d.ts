fed3fef8f0225196e8988a488b9dec2cedfa7102
三咲智子 <sxzz@sxzz.moe>
2022-08-27 03:14:00 +0800

    chore: remove v-loading dts

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -1,4 +1,3 @@
-import type { ElLoadingDirective } from 'element-plus'
 import type { vShow } from 'vue'
 
 declare module '@vue/runtime-core' {
@@ -8,7 +7,6 @@ declare module '@vue/runtime-core' {
 
   export interface ComponentCustomProperties {
     vShow: typeof vShow
-    vLoading: typeof ElLoadingDirective
   }
 }
 
